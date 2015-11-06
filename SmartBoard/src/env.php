29273d9d391f68c737007fcec82f8e222b9aafcc
aso1301138 <kanchikuryou@kanchikuryou-no-MacBook-Air.local>
2015-11-06 15:14:57 +0900

    a
    a

--- SmartBoard/src/env.php
+++ SmartBoard/src/env.php
@@ -2,6 +2,6 @@
 
 define('CONSUMER_KEY', 'PTn6fVX7wKvUDABkec5M0QKlK');
 define('CONSUMER_SECRET', 'TeZbftHWUXvWoyHEKyIotVeNSLovgazCbRkZ8MikzPJ8yO3MuW');
-define('OAUTH_CALLBACK', 'http://smartboard.webcrow.jp/html/top.html');
+define('OAUTH_CALLBACK', 'http://smartboard.webcrow.jp/src/callback.php');
 
 ?>
